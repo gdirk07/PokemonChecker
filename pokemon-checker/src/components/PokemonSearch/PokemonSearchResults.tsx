e12e690ffebe43f139dd46ebc0a91d650346254c
Jeremy <y2kfirzen@gmail.com>
2021-10-23 15:32:33 -0700

    Addressed inconsistent whitespace in search result

--- pokemon-checker/src/components/PokemonSearch/PokemonSearchResults.tsx
+++ pokemon-checker/src/components/PokemonSearch/PokemonSearchResults.tsx
@@ -14,24 +14,24 @@ type PokemonSearchResultsProps = {
 
 const PokemonSearchResults = ({pokemonQuery, onPokemonSelected}: PokemonSearchResultsProps) => {
   let pokemonComponent: JSX.Element[] = [];
-  if (pokemonQuery){
-      pokemonComponent = pokemonQuery.map((pokemon, p) =>
+  if (pokemonQuery) {
+    pokemonComponent = pokemonQuery.map((pokemon, p) =>
       <PokemonId key={p} name={pokemon.name} url={pokemon.url} onSelectPokemon={onPokemonSelected}></PokemonId>
-  )
+    )
   }
   if (pokemonComponent.length === 0) {
-      return (
-          <div>
-              <h4>No pokemon found</h4>
-          </div>
-      )
+    return (
+      <div>
+          <h4>No pokemon found</h4>
+      </div>
+    )
   }
   else {
-      return (
-          <div className="pokemonList">
-                  {pokemonComponent}
-          </div>
-      );
+    return (
+      <div className="pokemonList">
+        {pokemonComponent}
+      </div>
+    );
   }
 }
 export default PokemonSearchResults;
