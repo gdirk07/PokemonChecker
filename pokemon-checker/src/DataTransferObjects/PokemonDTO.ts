1027beee25a3aac6a0f0c4652d81772bc16177e8
gdirk07 <gdirk07@gmail.com>
2021-11-02 22:54:47 -0700

    Updated PokemonDTO to include Stats
    Updated the DTO to include stats (left a comment about how we assign stats), added function to calculate total stats
    
    Updated display to show the total stats, started the process to show a table of the 6 base stats however a discussion needs to be had on how we use PokemonDisplay.tsx to render the view otherwise we will have a massive render block of HTML

--- pokemon-checker/src/DataTransferObjects/PokemonDTO.ts
+++ pokemon-checker/src/DataTransferObjects/PokemonDTO.ts
@@ -4,6 +4,9 @@ type PokemonConstructorOptions = {
   types: { type: { name: string } }[];
   moves: { name: string; url: string }[];
   sprites: { front_shiny: string; front_default: string };
+  stats: {
+    base_stat: number
+  }[]
 };
 
 /**
@@ -19,6 +22,15 @@ class PokemonDTO {
   public moves:{ name: string; url: string }[]; //TODO: Update this to a moveDTO[] object when created
   public frontDefault: string;
   public frontShiny: string;
+  public stats: {
+    hp: number,
+    attack: number,
+    defense: number,
+    spAttack: number,
+    spDefense: number,
+    speed: number
+  }
+  public baseStats: number //A cumulation of the values of the 6 stats
 
   constructor(pokemonConstructorOptions: PokemonConstructorOptions) {
     this.name = pokemonConstructorOptions.name;
@@ -30,6 +42,26 @@ class PokemonDTO {
       : null;
     this.frontDefault = pokemonConstructorOptions.sprites.front_default;
     this.frontShiny = pokemonConstructorOptions.sprites.front_shiny;
+
+    //TODO: there has to be a more structured method, this relies on the consistency of the API to map.
+    this.stats = {
+      hp: pokemonConstructorOptions.stats[0].base_stat,
+      attack: pokemonConstructorOptions.stats[1].base_stat,
+      defense: pokemonConstructorOptions.stats[2].base_stat,
+      spAttack: pokemonConstructorOptions.stats[3].base_stat,
+      spDefense: pokemonConstructorOptions.stats[4].base_stat,
+      speed: pokemonConstructorOptions.stats[5].base_stat
+    };
+    this.baseStats = this.calculateBaseStats();
+  }
+
+  private calculateBaseStats(): number {
+    if (this.stats)
+    {
+      const s = this.stats;
+      return s.hp + s.attack + s.defense + s.spAttack + s.spDefense + s.speed;
+    }
+    return 0;
   }
 }
 
