f75f17735aa17cccce1c8a50ef0be439a4df69f4
Jeremy <jeremy-jtlo@users.noreply.github.com>
2022-01-14 16:21:16 -0800

    Moved unit test data to stubs
    Some of these unit tests are going to be relying on some payloads that we'll want to manage in one place.

--- pokemon-checker/src/DataTransferObjects/PokemonDTO.test.ts
+++ pokemon-checker/src/DataTransferObjects/PokemonDTO.test.ts
@@ -1,12 +1,9 @@
+import { IPokemonStub } from "../interfaces/PokemonData";
+import PokemonDTO from "./PokemonDTO";
 import {
-  IPokemonStub,
-  ITypeData,
-} from "../interfaces/PokemonData";
-import PokemonDTO, {
-  ElementType,
-  PokemonConstructorOptions,
-} from "./PokemonDTO";
-import { MoveDTO } from "./MoveDTO";
+  heraConstructorOpts,
+  getEmptyPokemonOpts,
+} from "../_stubs/PokemonData";
 
 test("Test creation of a pokemon stub", () => {
   const testStub: IPokemonStub = {
@@ -14,37 +11,7 @@ test("Test creation of a pokemon stub", () => {
     url: "https://test.test/",
   };
 
-  const dummyType: ITypeData = {
-    name: ElementType.BUG,
-    url: "https://pokeapi.co/api/v2/type/7/",
-  };
-  const opts: PokemonConstructorOptions = {
-    name: testStub.name,
-    id: -1,
-    types: [dummyType],
-    sprites: {
-      back_default: null,
-      back_female: null,
-      back_shiny: null,
-      back_shiny_female: null,
-      front_default: "",
-      front_female: null,
-      front_shiny: "",
-      front_shiny_female: null,
-      other: {},
-      versions: {},
-    },
-    moves: [],
-    stats: [
-      { base_stat: 0 },
-      { base_stat: 0 },
-      { base_stat: 0 },
-      { base_stat: 0 },
-      { base_stat: 0 },
-      { base_stat: 0 },
-    ],
-    url: testStub.url,
-  };
+  const opts = getEmptyPokemonOpts(testStub.name, testStub.url);
 
   const testPokemon = new PokemonDTO(opts);
 
@@ -55,62 +22,7 @@ test("Test creation of a pokemon stub", () => {
 });
 
 test("Test creation of a valid pokemon object", () => {
-  const heraStub: PokemonConstructorOptions = {
-    id: 214,
-    moves: [
-      new MoveDTO({
-        name: "swords-dance",
-        url: "https://pokeapi.co/api/v2/move/14/",
-      }),
-    ],
-    name: "heracross",
-    sprites: {
-      back_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/214.png",
-      back_female:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/female/214.png",
-      back_shiny:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/214.png",
-      back_shiny_female:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/female/214.png",
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/214.png",
-      front_female:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/214.png",
-      front_shiny:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/214.png",
-      front_shiny_female:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/female/214.png",
-      other: {},
-      versions: {},
-    },
-    stats: [
-      {
-        base_stat: 80, // HP
-      },
-      {
-        base_stat: 125, // ATK
-      },
-      {
-        base_stat: 75, // DEF
-      },
-      {
-        base_stat: 40, // SPA
-      },
-      {
-        base_stat: 95, // SPD
-      },
-      {
-        base_stat: 85, // SPE
-      },
-    ],
-    types: [
-      { name: ElementType.BUG, url: "https://pokeapi.co/api/v2/type/7/" },
-      { name: ElementType.FIGHTING, url: "https://pokeapi.co/api/v2/type/2/" },
-    ],
-  };
-
-  const testPokemon = new PokemonDTO(heraStub);
+  const testPokemon = new PokemonDTO(heraConstructorOpts);
 
   expect(testPokemon.name).toBe("heracross");
   expect(testPokemon.url).toBe("");
---
+++ pokemon-checker/src/_stubs/PokemonData.ts
@@ -0,0 +1,104 @@
+import {
+  ElementType,
+  PokemonConstructorOptions,
+} from "../DataTransferObjects/PokemonDTO";
+import { MoveDTO } from "../DataTransferObjects/MoveDTO";
+
+/**
+ * Input object for creating a pokemon DTO
+ */
+export const heraConstructorOpts: PokemonConstructorOptions = {
+  id: 214,
+  moves: [
+    new MoveDTO({
+      name: "swords-dance",
+      url: "https://pokeapi.co/api/v2/move/14/",
+    }),
+  ],
+  name: "heracross",
+  sprites: {
+    back_default:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/214.png",
+    back_female:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/female/214.png",
+    back_shiny:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/214.png",
+    back_shiny_female:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/female/214.png",
+    front_default:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/214.png",
+    front_female:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/214.png",
+    front_shiny:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/214.png",
+    front_shiny_female:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/female/214.png",
+    other: {},
+    versions: {},
+  },
+  stats: [
+    {
+      base_stat: 80, // HP
+    },
+    {
+      base_stat: 125, // ATK
+    },
+    {
+      base_stat: 75, // DEF
+    },
+    {
+      base_stat: 40, // SPA
+    },
+    {
+      base_stat: 95, // SPD
+    },
+    {
+      base_stat: 85, // SPE
+    },
+  ],
+  types: [
+    { name: ElementType.BUG, url: "https://pokeapi.co/api/v2/type/7/" },
+    { name: ElementType.FIGHTING, url: "https://pokeapi.co/api/v2/type/2/" },
+  ],
+};
+
+/**
+ * Supporting data needed to generate from a stub
+ * @param pokemonName The name of the pokemon
+ * @param pokemonUrl String for the full lookup of the pokemon
+ */
+export const getEmptyPokemonOpts = (
+  pokemonName: string,
+  pokemonUrl: string
+): PokemonConstructorOptions => ({
+  name: pokemonName,
+  id: -1,
+  types: [
+    {
+      name: ElementType.BUG,
+      url: "https://pokeapi.co/api/v2/type/7/",
+    },
+  ],
+  sprites: {
+    back_default: null,
+    back_female: null,
+    back_shiny: null,
+    back_shiny_female: null,
+    front_default: "",
+    front_female: null,
+    front_shiny: "",
+    front_shiny_female: null,
+    other: {},
+    versions: {},
+  },
+  moves: [],
+  stats: [
+    { base_stat: 0 },
+    { base_stat: 0 },
+    { base_stat: 0 },
+    { base_stat: 0 },
+    { base_stat: 0 },
+    { base_stat: 0 },
+  ],
+  url: pokemonUrl,
+});
