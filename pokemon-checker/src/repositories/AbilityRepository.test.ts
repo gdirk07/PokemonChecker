443dae343d8117f35899d238cfed2d37bbc2b49d
gdirk07 <gdirk07@gmail.com>
2022-04-18 01:20:22 -0700

    Update AbilityRepository.test.ts
    Modified suite to be more independent.

--- pokemon-checker/src/repositories/AbilityRepository.test.ts
+++ pokemon-checker/src/repositories/AbilityRepository.test.ts
@@ -3,15 +3,13 @@ import { pressureAbility } from "../_stubs/AbilityData";
 
 const repo: AbilityRepository = new AbilityRepository();
 
-beforeAll(() => {
-  repo.setAbilityData(pressureAbility);
-});
-
 test("Test creation of a repository item", () => {
+  repo.setAbilityData(pressureAbility);
   expect(repo.getAbility(pressureAbility.name)).toBe(pressureAbility);
 });
 
 test("Test expiry function not expired", () => {
+  repo.setAbilityData(pressureAbility);
   expect(repo.isExpired(pressureAbility.name)).toBe(false);
 });
 
