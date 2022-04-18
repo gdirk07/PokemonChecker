8c08734a669f294a1801d97915386848917a26d9
gdirk07 <gdirk07@gmail.com>
2022-04-17 17:28:27 -0700

    Update AbilityRepository.test.ts
    Updated false positive clause because we need to mock localstorage properly so clearing storage did nothing. Keeping tests as is because these tests are just to make sure we're setting data, loading data, and checking expiry based on the table entry not so much testing localstorage itself.

--- pokemon-checker/src/repositories/AbilityRepository.test.ts
+++ pokemon-checker/src/repositories/AbilityRepository.test.ts
@@ -1,22 +1,17 @@
 import { AbilityRepository } from "./AbilityRepository";
 import { pressureAbility } from "../_stubs/AbilityData";
 
-let repo: AbilityRepository;
-beforeAll(() => {
-  repo = new AbilityRepository();
-});
+const repo: AbilityRepository = new AbilityRepository();
 
-afterEach(() => {
-  localStorage.clear();
+beforeAll(() => {
+  repo.setAbilityData(pressureAbility);
 });
 
 test("Test creation of a repository item", () => {
-  repo.setAbilityData(pressureAbility);
   expect(repo.getAbility(pressureAbility.name)).toBe(pressureAbility);
 });
 
 test("Test expiry function not expired", () => {
-  repo.setAbilityData(pressureAbility);
   expect(repo.isExpired(pressureAbility.name)).toBe(false);
 });
 
